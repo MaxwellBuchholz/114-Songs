e0f02811b9a3b4f277efbdf237ccf53364f9ad8f
Maxwell Buchholz <149419880+MaxwellBuchholz@users.noreply.github.com>
2026-05-20 10:40:25 -0700

    Updated convert.sh

--- convert.sh
+++ convert.sh
@@ -1,10 +1,34 @@
 #!/bin/bash
-# Usage: ./convert.sh /MuseScore/file.mscz
 # There's proably a better way to do this but it works.
 
-MSCZ="$1"
 
-PDF=$( echo $MSCZ | sed 's/.mscz/.pdf/')
-PDF=$( echo $PDF | sed 's/MuseScore/pdf/')
+if  [[ $1 = "-f" ]]; then
+	MSCZ="$2"
 
-mscore -o $PDF $MSCZ
+	PDF=$( echo $MSCZ | sed 's/.mscz/.pdf/')
+	PDF=$( echo $PDF | sed 's/MuseScore/pdf/')
+
+	mscore -o $PDF $MSCZ
+
+elif  [[ $1 = "-a" ]]; then
+	for MSCZ in MuseScore/*.mscz; do
+
+		PDF=$( echo $MSCZ | sed 's/.mscz/.pdf/')
+		PDF=$( echo $PDF | sed 's/MuseScore/pdf/')
+
+		mscore -o $PDF $MSCZ
+	done
+
+elif  [[ $1 = "-c" ]]; then
+	#TODO: Combine with multiple scores on the same page (e.g. Cradle song and La Fede)
+	pdfunite pdf/*.pdf 114-songs.pdf
+
+else
+	echo "Usage:"
+	echo "  -f [FILE]"
+	echo "     Selects mscz file to convert to pdf"
+	echo "  -a"
+	echo "     Converts all mscz files to pdfs"
+	echo "  -c"
+	echo "     Combines all pdf files into one pdf"
+fi
